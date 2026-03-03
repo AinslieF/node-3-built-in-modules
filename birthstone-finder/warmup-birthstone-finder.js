a2db25e06d8d554d9bafcb711c1fdba413f08747
AinslieF <ainsliefrancisdec16@gmail.com>
2026-03-03 18:02:32 -0500

    Completed Birthstone Finder

--- birthstone-finder/warmup-birthstone-finder.js
+++ birthstone-finder/warmup-birthstone-finder.js
@@ -21,3 +21,23 @@ const birthstones = {
   November: "Topaz or Citrine",
   December: "Tanzanite, Zircon, or Turquoise",
 };
+
+// Gets the month that the user typed in the terminal
+let month = process.argv[2];
+
+// Looks up that month inside the birthstones object
+// If the month exists, then stone will hold the birthstone
+// If it does not exist, then stone will be undefined
+let stone = birthstones[month];
+
+// Checks if the month was found in the object
+if (stone) {
+
+  // If the month is valid, log the full sentence
+  console.log("The birthstone for " + month + " is " + stone + ".");
+}
+else {
+
+  // If the month is not valid, log an error message
+  console.log("Invalid month. Please enter a valid month.");
+}
